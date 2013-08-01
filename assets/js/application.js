c39c8ce233b4139d5f248a6a1ee6a141e3da8e57
CaptainCodeman <simon@captaincodeman.com>
2013-08-01 09:55:27 -0600

    Fix side-nav affix

--- assets/js/application.js
+++ assets/js/application.js
@@ -29,6 +29,26 @@
         navHeight + 5 // offset scroll by nav
     })
 
+    // back to top
+    setTimeout(function () {
+      var $sideBar = $('.bs-sidebar')
+
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var navOuterHeight = $('.bs-docs-nav').height()
+
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+          }
+        , bottom: function () {
+            return (this.bottom = $('.bs-footer').outerHeight(true))
+          }
+        }
+      })
+    }, 100)
+
     // tooltip demo
     $('.tooltip-demo').tooltip({
       selector: "[data-toggle=tooltip]"
